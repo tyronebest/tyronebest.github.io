e53d1be1aad2f448cb5ecafcbdae0214fb98117b
tyronebest <15best7961@pit.edu>
2016-11-22 16:22:43 +0000

    one button

--- mobilefall2016/onebutton/script.js
+++ mobilefall2016/onebutton/script.js
@@ -6,21 +6,16 @@ mouseover
 mouseout
 */
 var btn = document.getElementById("btn");
-var msg;
+var msg = document.getElementById("msg");
 
 //the event callback function
 
 function eventCallback(e) {
     msg.innerHTML = e.type;
-}
+};
 
 //attach event handlers to the button
 //for each of the four events mentioned above 
- btn.addEventListener("mousedown", function(event) {
-    if (event.which == 1)
-      console.log("Left button");
-    else if (event.which == 2)
-      console.log("Middle button");
-    else if (event.which == 3)
-      console.log("Right button");
+btn.addEventListener("mousedown", function() {
+eventCallback("mouse down was clicked");
   });
