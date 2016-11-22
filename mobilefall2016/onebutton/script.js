74be9939f84b59369d6977d033ac12f3eeda9373
tyronebest <15best7961@pit.edu>
2016-11-22 16:13:41 +0000

    one button

--- mobilefall2016/onebutton/script.js
+++ mobilefall2016/onebutton/script.js
@@ -16,4 +16,11 @@ function eventCallback(e) {
 
 //attach event handlers to the button
 //for each of the four events mentioned above 
-btn.addEventListener("mousedown", eventCallback, false);
+ btn.addEventListener("mousedown", function(event) {
+    if (event.which == 1)
+      console.log("Left button");
+    else if (event.which == 2)
+      console.log("Middle button");
+    else if (event.which == 3)
+      console.log("Right button");
+  });
