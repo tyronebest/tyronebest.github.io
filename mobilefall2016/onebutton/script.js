ea23503d93ebb03c31fd0642d74efc5081f7b6f9
tyronebest <15best7961@pit.edu>
2016-11-22 15:45:56 +0000

    one button

---
+++ mobilefall2016/onebutton/script.js
@@ -0,0 +1,21 @@
+
+/*
+event handler for the button's for events :
+mousedown
+mouseup
+mouseover
+mouseout
+*/
+var btn = document.getElementById("btn");
+var msg;
+
+//the event callback funtion
+
+function eventCallback(e){
+    msg.innerHTML = e.type;
+}
+
+//attach event handlers to the button
+//for each of the four events mentioned above 
+btn.addEventListener("mousedown" ,eventCallback(msg)
+    );
