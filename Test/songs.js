bf789be88a220dd28eabf51c1fae0d3a960ae9d3
tyronebest <15best7961@pit.edu>
2016-12-20 01:13:25 +0000

    first

---
+++ Test/songs.js
@@ -0,0 +1,19 @@
+/*
+	Each song is an .mp3
+	Each picture is a .png
+*/
+var songs = {
+	1:{
+		song:"Logic - 44 Bars",
+		picture: "logicc"
+	},
+	'2':{
+		song:"Lil Yachty - 1Night",
+		picture: "lilboat"
+	},
+	3:{
+		song:"Wiz Khalifa - Pull Up (ft. Lil Uzi Vert)",
+		picture: "wiz"
+	}
+};
+
